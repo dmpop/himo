2650ed56583ab1c6803ea616c988aa2422e7109e
Dmitri Popov <dmpop@linux.com>
2020-12-26 16:47:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Sigh
+
+Sigh provides a simple web interface to control any supported camera via gPhoto2.
+
+## Dependencies
+
+- PHP
+- gPhoto2
+- Git (optional)
+
+## Installation and Usage
+
+1. Install the required packages on a local machine.
+2. Clone the project's repository using the `git clone https://github.com/dmpop/sigh.git` command. Alternatively, download the latest source code using the appropriate button on the project's pages.
+3. Connect your camera to the machine, and turn the camera on.
+3. Switch in the terminal to the _sigh_ directory and run the `php -S 0.0.0.0:8000` command.
+4. Point the browser to the _127.0.0.1:8000_ address.
+
+The [Linux Photography](https://gumroad.com/l/linux-photography) book provides detailed information  on creating Hald CLUT presets for use with Lilut. Get your copy at [Google Play Store](https://play.google.com/store/books/details/Dmitri_Popov_Linux_Photography?id=cO70CwAAQBAJ) or [Gumroad](https://gumroad.com/l/linux-photography).
+
+<img src="https://i.imgur.com/wBgcfSk.jpg" title="Linux Photography book" width="200"/>
+
+## Problems?
+
+Please report bugs and issues in the [Issues](https://github.com/dmpop/sigh/issues) section.
+
+## Contribute
+
+If you've found a bug or have a suggestion for improvement, open an issue in the [Issues](https://github.com/dmpop/sigh/issues) section.
+
+To add a new feature or fix issues yourself, follow the following steps.
+
+1. Fork the project's repository
+2. Create a feature branch using the `git checkout -b new-feature` command
+3. Add your new feature or fix bugs and run the `git commit -am 'Add a new feature'` command to commit changes
+4. Push changes using the `git push origin new-feature` command
+5. Submit a pull request
+
+## Author
+
+Dmitri Popov [dmpop@linux.com](mailto:dmpop@linux.com)
+
+## License
+
+The [GNU General Public License version 3](http://www.gnu.org/licenses/gpl-3.0.en.html)
+ 
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Author: Dmitri Popov, dmpop@linux.com
+	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
+
+<head>
+	<meta charset="utf-8">
+	<title>Sigh</title>
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="water.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+	<img style="display: inline; height: 1.6em;" src="favicon.svg" alt="logo" />
+	<h1 style="display: inline; height: 2em; margin-left: 0.3em; letter-spacing: 3px; color: rgb(200, 113, 55);">SIGH</h1>
+
+	<form style="margin-top: 1em;" action='index.php' method='POST'>
+		<select name='parameter'>
+			<option value=''>Select command</option>
+			<option value='--capture-image-and-download --filename photos/%Y%m%d-%H%M%S-%03n.%C'>Capture and download</option>
+			<option value='--abilities'>Show camera's abilities</option>
+			<option value='--list-config'>List configurable parameters</option>
+			<option value='--version'>Version</option>
+			<option value='--help'>Help</option>
+		</select>
+		<p>Aperture:</p>
+		<input style="margin-bottom: 1.5em;" type="text" name="fn">
+		<p>ISO:</p>
+		<input style="margin-bottom: 1.5em;" type="text" name="iso">
+		<p>gPhoto2 command <em style="color:lightgray">(example: --list-config)</em>:</p>
+		<input style="margin-bottom: 1.5em;" type="text" name="cmd">
+		<input type='submit' value='OK' />
+	</form>
+
+	<?php
+
+	if (!file_exists("photos")) {
+		mkdir("photos", 0777, true);
+	}
+
+	if (!empty($_POST["fn"])) {
+		echo '<pre>';
+		passthru("gphoto2 --set-config f-number=" . $_POST["fn"]);
+		echo '</pre>';
+	}
+
+	if (!empty($_POST["iso"])) {
+		echo '<pre>';
+		passthru("gphoto2 --set-config iso=" . $_POST["iso"]);
+		echo '</pre>';
+	}
+
+	if (!empty($_POST["cmd"])) {
+		echo '<pre>';
+		passthru('gphoto2 ' . $_POST["cmd"]);
+		echo '</pre>';
+	}
+
+	if (isset($_POST["parameter"])) {
+		$command = 'gphoto2 ' . $_POST['parameter'];
+		echo '<pre>';
+		passthru("$command");
+		echo '</pre>';
+	}
+	?>
+
+</body>
+
+</html>
