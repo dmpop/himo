2549a2b69eab9f8d59bfe107fbaf14e34dad2443
Dmitri Popov <dmpop@linux.com>
2021-03-10 15:03:38 +0100

    Switch to Classless CSS

--- edit.php
+++ edit.php
@@ -10,7 +10,7 @@ error_reporting(E_ERROR);
     <meta charset="utf-8">
     <title>ひも</title>
     <link rel="shortcut icon" href="img/favicon.png" />
-    <link rel="stylesheet" href="water.css">
+    <link rel="stylesheet" href="classless.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         textarea {
@@ -19,39 +19,45 @@ error_reporting(E_ERROR);
             height: 55%;
             line-height: 1.9;
             margin-top: 2em;
+            margin-bottom: 2em;
         }
     </style>
 </head>
 
 <body>
-    <img style="display: inline; height: 2em; vertical-align: middle;" src="img/favicon.png" alt="logo" />
-    <h1 style="display: inline; margin-left: 0.19em; letter-spacing: 3px; color: rgb(200, 113, 55); vertical-align: middle;">HIMO</h1>
-    <hr style="margin-bottom: 2em;">
-    <button style="display: inline;" onclick='window.location.href = "index.php"'>Back</button>
-    <?php
-    function Read()
-    {
-        $f = "commands.csv";
-        echo file_get_contents($f);
-    }
-    function Write()
-    {
-        $f = "commands.csv";
-        $fp = fopen($f, "w");
-        $data = $_POST["text"];
-        fwrite($fp, $data);
-        fclose($fp);
-    }
-    if ($_POST["save"]) {
-        Write();
-    };
-    ?>
-    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-        <textarea name="text"><?php Read(); ?></textarea><br /><br />
-        <input style="background-color: #ccffcc;" type="submit" name="save" value="Save">
-    </form>
-    <hr style="margin-top: 2em;">
-    <p>This is <a href="https://github.com/dmpop/himo">Himo</a></p>
+    <div class="text-center">
+        <h1 class="text-center" style="letter-spacing: 3px;">HIMO</h1>
+        <hr style="margin-bottom: 2em;">
+        <button style="display: inline;" onclick='window.location.href = "index.php"'>Back</button>
+        <?php
+        function Read()
+        {
+            $f = "commands.csv";
+            echo file_get_contents($f);
+        }
+        function Write()
+        {
+            $f = "commands.csv";
+            $fp = fopen($f, "w");
+            $data = $_POST["text"];
+            fwrite($fp, $data);
+            fclose($fp);
+            echo "<script>";
+            echo 'alert("Changes saved.")';
+            echo "</script>";
+        }
+        if (isset($_POST["save"])) {
+            Write();
+        };
+        ?>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            <textarea name="text"><?php Read(); ?></textarea>
+            <!-- <input type="submit" style="background-color: #ccffcc;" name="save" value="Save"/> -->
+            <button type="submit" style="background-color: #ccffcc;" name="save">Save</button>
+        </form>
+        <hr style="margin-top: 2em;">
+        <p>This is <a href="https://github.com/dmpop/himo">Himo</a></p>
+    </div>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<meta charset="utf-8">
 	<title>ひも</title>
 	<link rel="shortcut icon" href="img/favicon.png" />
-	<link rel="stylesheet" href="water.css">
+	<link rel="stylesheet" href="classless.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -18,112 +18,105 @@
 			window.history.replaceState(null, null, window.location.href);
 		}
 	</script>
+	<div class="text-center">
+		<h1 class="text-center" style="letter-spacing: 3px;">HIMO</h1>
+		<hr style="margin-bottom: 2em;">
 
-	<img style="display: inline; height: 2em; vertical-align: middle;" src="img/favicon.png" alt="logo" />
-	<h1 style="display: inline; margin-left: 0.19em; letter-spacing: 3px; color: rgb(200, 113, 55); vertical-align: middle;">HIMO</h1>
-	<hr style="margin-bottom: 2em;">
+		<?php
+		// Check is the camera is connected
+		$CAMERA = shell_exec("gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//'");
+		// If the camera is not detected, show a warning message
+		if (empty($CAMERA)) {
+			echo '<img style="display: inline; height: 1.5em; margin-right: 0.5em; vertical-align: middle;" src="img/alert.svg" alt="alert" />';
+			echo 'Camera is not connected.';
+		}
+		// Iff the camera is detected, capture a preview image
+		if (!empty($_POST["refresh"])) {
+			unlink("capture_preview.jpg");
+			shell_exec("gphoto2 --capture-preview");
+		}
+		echo "</p>";
+		// Show the captured preview image
+		if (file_exists("capture_preview.jpg")) {
+			echo '<img style="border-radius: 9px;" src="capture_preview.jpg">';
+		}
+		?>
 
-	<?php
-	// Display the IP address of the machine running Himo
-	echo '<p>';
-	echo "IP address: <strong>";
-	passthru('hostname -I');
-	echo '</strong></p>';
-	echo "<p style='margin-top: 2em;'>";
+		<?php
+		// Read the first row of the commands.csv file
+		if (file_exists("commands.csv")) {
+			$handle = fopen("commands.csv", "r");
+			$custom = fgetcsv($handle, 1000, ";");
+		}
+		?>
 
-	// Check is the camera is connected
-	$CAMERA = shell_exec("gphoto2 --auto-detect | grep usb | cut -b 36-42 | sed 's/,/\//'");
-	// If the camera is not detected, show a warning message
-	if (empty($CAMERA)) {
-		echo '<img style="display: inline; height: 1.5em; margin-right: 0.5em; vertical-align: middle;" src="img/alert.svg" alt="alert" />';
-		echo 'Camera is not connected.';
-	}
-	// Iff the camera is detected, capture a preview image
-	if (!empty($_POST["refresh"])) {
-		unlink("capture_preview.jpg");
-		shell_exec("gphoto2 --capture-preview");
-	}
-	echo "</p>";
-	// Show the captured preview image
-	if (file_exists("capture_preview.jpg")) {
-		echo '<img style="border-radius: 9px;" src="capture_preview.jpg">';
-	}
-	?>
+		<form style="margin-top: 2em;" action=' ' method='POST'>
+			<button type="submit" style="display: inline;" name="refresh">Refresh</button>
+			<button type="submit" style="background-color: #cce6ff;  display: inline;" name="capture">Capture</button>
+			<!-- Custom button that uses the first value of the $custom array as the button label -->
+			<button type="submit" style="display: inline;" name="custom"><?php echo $custom[0] ?></button>
+		</form>
 
-	<?php
-	// Read the first row of the commands.csv file
-	if (file_exists("commands.csv")) {
-		$handle = fopen("commands.csv", "r");
-		$custom = fgetcsv($handle, 1000, ";");
-	}
-	?>
+		<?php
+		if (!empty($_POST["capture"])) {
+			echo "<pre>";
+			passthru("gphoto2 --capture-image-and-download --keep --filename photos/%Y%m%d-%H%M%S-%03n.%C");
+			echo "</pre>";
+		}
+		?>
 
-	<form style="margin-top: 2em;" action=' ' method='POST'>
-		<input style="display: inline;" type='submit' name='refresh' value='Refresh' />
-		<input style="background-color: #cce6ff;  display: inline;" type='submit' name='capture' value='Capture' />
-		<!-- Custom button that uses the first value of the $custom array as the button label -->
-		<input style="background-color: #ccffcc; display: inline;" type='submit' name='custom' value='<?php echo $custom[0] ?>' />
-	</form>
+		<?php
+		// If the custom button pressed, pass the second value of the $custom array as a gPhoto2 command.
+		if (!empty($_POST["custom"])) {
+			echo "<pre>";
+			passthru('gphoto2 ' . $custom[1]);
+			echo "</pre>";
+		}
+		?>
 
-	<?php
-	if (!empty($_POST["capture"])) {
-		echo "<pre>";
-		passthru("gphoto2 --capture-image-and-download --keep --filename photos/%Y%m%d-%H%M%S-%03n.%C");
-		echo "</pre>";
-	}
-	?>
-
-	<?php
-	// If the custom button pressed, pass the second value of the $custom array as a gPhoto2 command.
-	if (!empty($_POST["custom"])) {
-		echo "<pre>";
-		passthru('gphoto2 ' . $custom[1]);
-		echo "</pre>";
-	}
-	?>
-
-	<form style="margin-top: 2em;" action='index.php' method='POST'>
-		<select name='parameter'>
-			<option value=''>Select command</option>
-			<?php
-			if (file_exists("commands.csv")) {
-				$handle = fopen("commands.csv", "r");
-				fgetcsv($handle);
-				while (($row = fgetcsv($handle, 1000, ";")) !== FALSE) {
-					echo '<option value="' . $row[1] . '">' . $row[0] . "</option>";
+		<form style="margin-top: 2em;" action='index.php' method='POST'>
+			<select name='parameter'>
+				<option value=''>Select command</option>
+				<?php
+				if (file_exists("commands.csv")) {
+					$handle = fopen("commands.csv", "r");
+					fgetcsv($handle);
+					while (($row = fgetcsv($handle, 1000, ";")) !== FALSE) {
+						echo '<option value="' . $row[1] . '">' . $row[0] . "</option>";
+					}
+				} else {
+					echo '<option disabled>commands.csv not found</option>';
 				}
-			} else {
-				echo '<option disabled>commands.csv not found</option>';
-			}
-			?>
-		</select>
-		<input style="margin-top: 1.5em;" type="button" onclick="location.href='edit.php';" value="Edit" />
-		<p>gPhoto2 parameters: <em style="color:lightgray">(example: --list-config)</em></p>
-		<input style="margin-bottom: 1.5em;" type="text" name="cmd">
-		<input style="background-color: #ccffcc; display: inline;" type='submit' value='OK' />
-	</form>
+				?>
+			</select>
+			<p>Optional gPhoto2 parameters: <em style="color:lightgray">(example: --list-config)</em></p>
+			<input type="text" name="cmd">
+			<button style="background-color: #ccffcc;" type='submit'>OK</button>
+		</form>
+		<button type='submit' style="margin-top: 1.5em; display: inline;" type="button" onclick="location.href='edit.php';">Edit commands</button>
 
-	<?php
+		<?php
 
-	if (!file_exists("photos")) {
-		mkdir("photos", 0777, true);
-	}
+		if (!file_exists("photos")) {
+			mkdir("photos", 0777, true);
+		}
 
-	if (!empty($_POST["cmd"])) {
-		echo '<pre>';
-		passthru('gphoto2 ' . $_POST["cmd"]);
-		echo '</pre>';
-	}
+		if (!empty($_POST["cmd"])) {
+			echo '<pre>';
+			passthru('gphoto2 ' . $_POST["cmd"]);
+			echo '</pre>';
+		}
 
-	if (isset($_POST["parameter"])) {
-		echo '<hr style="margin-top: 2em;"><pre>';
-		$command = 'gphoto2 ' . $_POST['parameter'];
-		passthru("$command");
-		echo '</pre>';
-	}
-	?>
-	<hr style="margin-top: 2em;">
-	<p>This is <a href="https://github.com/dmpop/himo">Himo</a></p>
+		if (isset($_POST["parameter"])) {
+			echo '<hr style="margin-top: 2em;"><pre>';
+			$command = 'gphoto2 ' . $_POST['parameter'];
+			passthru("$command");
+			echo '</pre>';
+		}
+		?>
+		<hr style="margin-top: 2em;">
+		<p>This is <a href="https://github.com/dmpop/himo">Himo</a></p>
+	</div>
 </body>
 
 </html>
